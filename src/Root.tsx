16dcf24fea190870dc4c60b9ff3963263127c213
Dima Spodeniuk <dima.spodeniuk@gmail.com>
2023-03-30 18:08:21 +0300

    feat(Root): added data loading and routes

---
+++ src/Root.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import api from "api";
+
+import { Menu } from "shared/components/ui";
+import { MenuType } from "shared/types";
+
+export const Root = () => {
+  const [menuList, setMenuList] = useState<MenuType | null>(null);
+
+  const fetchMenuData = async () => {
+    try {
+      const { data } = await api.services.menu.getItems();
+      setMenuList(data);
+    } catch(e) {}
+  }
+
+  useEffect(() => {
+    fetchMenuData()
+  }, [])
+
+  if(!menuList) return null;
+
+  return (
+    <Routes>
+      {menuList.menuItems.data.map(item => <Route key={`page-route${item.id}`} path={item.attributes.url} element={<Menu data={menuList} />} />)}
+    </Routes>
+  )
+}
