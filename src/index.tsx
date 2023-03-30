7528449db2f3311f667a3ba4880078f97aa4a5a2
Dima Spodeniuk <dima.spodeniuk@gmail.com>
2023-03-30 18:07:59 +0300

    feat: app providers & global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+
 import reportWebVitals from './reportWebVitals';
+
+import { Root } from './Root';
+
+import api from 'api';
+
+import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ApolloProvider client={api.client}>
+        <Root />
+      </ApolloProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
